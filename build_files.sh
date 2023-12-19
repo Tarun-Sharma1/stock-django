dd02d77b3df4fd25f3523109f312a48c4fec3bd2
Tarun Sharma <76693984+Tarun-Sharma1@users.noreply.github.com>
2023-12-19 05:47:52 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
+# Use Python 3.10
+python_version="3.10.0"
+
+# Install or upgrade Python using pyenv
+pyenv install $python_version
+pyenv global $python_version
+
+# Install project dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+
+# Run collectstatic
+python manage.py collectstatic --noinput --clear
