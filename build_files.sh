0a0d04773344ae7a4ac2a9d53547deef521bcd35
Tarun Sharma <76693984+Tarun-Sharma1@users.noreply.github.com>
2023-12-19 06:17:16 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# Install pyenv (if not already installed)
-curl https://pyenv.run | bash
+# Activate your virtual environment if needed
+# source /path/to/your/virtualenv/bin/activate
 
-# Add pyenv to the PATH
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
-
-# Use Python 3.10
-python_version="3.10.0"
-
-# Install or upgrade Python using pyenv
-pyenv install $python_version
-pyenv global $python_version
-
-# Install project dependencies
+# Install dependencies
 pip install -r requirements.txt
 
-# Run collectstatic
-python manage.py collectstatic --noinput --clear
+# Run Django migrations
+python manage.py migrate
+
+# Start the Django development server
+python manage.py runserver 0.0.0.0:$PORT
